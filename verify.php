4349ea05693af63e4f989cfe4e06cf896bb559a1
thanadon2541 <62472512+thanadon2541@users.noreply.github.com>
2020-10-13 11:10:00 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'gEP1ipvGGe7y9z/OD5qWsCTocFG/rOVQ3Gp1mlmXKqx4omWPQTkjWOHNzqjQP80ucbLwejiCOKKgegIoAbWfNC0cwt381R6QkxVoyLDnDN48xtw6mNo/QfgHVTqdztTh3SsHtGSIH3AgRvYrsuprzAdB04t89/1O/w1cDnyilFU=';
 
 
 $url = 'https://api.line.me/v1/oauth/verify';
